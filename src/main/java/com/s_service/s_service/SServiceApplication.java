4d36c0851558eba7ae90d1d419cb61612b6a9a3a
quocthai <lxlthailxl@gmail.com>
2024-09-24 13:13:56 +0700

    init backend s-service webapp

---
+++ src/main/java/com/s_service/s_service/SServiceApplication.java
@@ -0,0 +1,13 @@
+package com.s_service.s_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s_service/s_service/SServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s_service.s_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

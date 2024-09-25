43b63044a25c53e358140f023b82dfdcb6d6288c
quocthai <lxlthailxl@gmail.com>
2024-09-25 09:23:31 +0700

    implement verify token

--- src/main/java/com/s_service/s_service/service/authentication/AuthenticationServiceImpl.java
+++ src/main/java/com/s_service/s_service/service/authentication/AuthenticationServiceImpl.java
@@ -1,11 +1,55 @@
 package com.s_service.s_service.service.authentication;
 
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.SignedJWT;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 @Service
-public class AuthenticationServiceImpl implements AuthenticationService{
-    @Override
-    public boolean introspect(String token) {
-        return true;
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Value("${jwt.signer-key}")
+    private String KEY;
+    @Value("${jwt.expiration-duration}")
+    private long EXPIRATION_DURATION;
+    @Value("${jwt.refreshable-duration}")
+    private String REFRESHABLE_DURATION;
+
+    private SignedJWT verifyToken(String token, boolean isRefresh) throws Exception {
+        JWSVerifier verifier = new MACVerifier(KEY.getBytes());
+        SignedJWT signedJWT = SignedJWT.parse(token);
+        Date expiryTime = (isRefresh) ? new Date(signedJWT.getJWTClaimsSet().getIssueTime().toInstant().plus(Long.parseLong(REFRESHABLE_DURATION), ChronoUnit.SECONDS).toEpochMilli()) : signedJWT.getJWTClaimsSet().getExpirationTime();
+        var verified = signedJWT.verify(verifier);
+        if (!(verified && expiryTime.after(new Date()))) {
+            throw new Exception("UNAUTHENTICATED");
+        }
+
+        if (isTokenInBlacklist(signedJWT.getJWTClaimsSet().getJWTID())) {
+            throw new Exception("UNAUTHENTICATED");
+        }
+        return signedJWT;
+    }
+
+    public boolean introspect(String token) throws Exception {
+        boolean isValid = true;
+        try {
+            verifyToken(token, false);
+
+        } catch (Exception e) {
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    private boolean isTokenInBlacklist(String jwtId) {
+        return redisTemplate.opsForValue().get("bl_" + jwtId) != null;
+    }
+
+    private void addTokenToBlacklist(String jwtId, Date expiryTime) {
+        redisTemplate.opsForValue().set("bl_" + jwtId, jwtId);
+        redisTemplate.expireAt("bl_" + jwtId, expiryTime);
     }
 }

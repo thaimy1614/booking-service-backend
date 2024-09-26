093af3cb34bc933631a819845816787609423363
quocthai <lxlthailxl@gmail.com>
2024-09-26 14:31:37 +0700

    add: profile mapper

---
+++ src/main/java/com/s_service/s_service/mapper/ProfileMapper.java
@@ -0,0 +1,10 @@
+package com.s_service.s_service.mapper;
+
+import com.s_service.s_service.dto.request.SignupRequest;
+import com.s_service.s_service.model.Profile;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProfileMapper {
+    Profile toProfile(SignupRequest request);
+}

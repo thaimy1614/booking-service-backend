e2bce0dd09dd79ad46670e58e72bea801e6ac103
quocthai <lxlthailxl@gmail.com>
2024-09-27 19:30:20 +0700

    add profile response mapper

--- src/main/java/com/s_service/s_service/mapper/ProfileMapper.java
+++ src/main/java/com/s_service/s_service/mapper/ProfileMapper.java
@@ -1,10 +1,13 @@
 package com.s_service.s_service.mapper;
 
 import com.s_service.s_service.dto.request.SignupRequest;
+import com.s_service.s_service.dto.response.ProfileResponse;
 import com.s_service.s_service.model.Profile;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ProfileMapper {
     Profile toProfile(SignupRequest request);
+
+    ProfileResponse toProfileResponse(Profile profile);
 }

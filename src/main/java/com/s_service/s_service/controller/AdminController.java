d07235f6758775d0459cfdcee5eb1dba50de28ef
quocthai <lxlthailxl@gmail.com>
2024-09-28 08:36:59 +0700

    admin: implement update user profile by user id

--- src/main/java/com/s_service/s_service/controller/AdminController.java
+++ src/main/java/com/s_service/s_service/controller/AdminController.java
@@ -2,16 +2,14 @@ package com.s_service.s_service.controller;
 
 
 import com.s_service.s_service.dto.ApiResponse;
+import com.s_service.s_service.dto.request.profile.UpdateProfileRequest;
 import com.s_service.s_service.dto.response.ProfileResponse;
 import com.s_service.s_service.service.profile.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -29,6 +27,18 @@ public class AdminController {
         Page<ProfileResponse> response = profileService.getAll(pageable);
         return ApiResponse.<Page<ProfileResponse>>builder()
                 .message("Get all profiles page:" + page + ", size:" + size + " successfully!")
+                .result(response)
+                .build();
+    }
+
+    @PutMapping("/user/{userId}")
+    ApiResponse<ProfileResponse> updateUserProfile(
+            @PathVariable("userId") String userId,
+            @RequestBody UpdateProfileRequest request
+    ) {
+        ProfileResponse response = profileService.updateProfile(request, userId);
+        return ApiResponse.<ProfileResponse>builder()
+                .message("Update profile information successfully!")
                 .result(response)
                 .build();
     }

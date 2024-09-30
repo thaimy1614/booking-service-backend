b0d503fab7c0e367bc8bb85bd042124b46d8714f
quocthai <lxlthailxl@gmail.com>
2024-09-30 14:21:59 +0700

    implement get all category with services

--- src/main/java/com/s_service/s_service/service/category/CategoryService.java
+++ src/main/java/com/s_service/s_service/service/category/CategoryService.java
@@ -1,8 +1,13 @@
 package com.s_service.s_service.service.category;
 
 import com.s_service.s_service.dto.request.category.CategoryCreationRequest;
+import com.s_service.s_service.dto.response.category.CategoryResponse;
 import com.s_service.s_service.dto.response.category.GetCategoryResponse;
+
+import java.util.List;
 
 public interface CategoryService {
     GetCategoryResponse addCategory(CategoryCreationRequest request);
+
+    List<CategoryResponse> getAllCategoriesWithServices();
 }
--- src/main/java/com/s_service/s_service/service/category/CategoryServiceImpl.java
+++ src/main/java/com/s_service/s_service/service/category/CategoryServiceImpl.java
@@ -1,12 +1,15 @@
 package com.s_service.s_service.service.category;
 
 import com.s_service.s_service.dto.request.category.CategoryCreationRequest;
+import com.s_service.s_service.dto.response.category.CategoryResponse;
 import com.s_service.s_service.dto.response.category.GetCategoryResponse;
 import com.s_service.s_service.mapper.CategoryMapper;
 import com.s_service.s_service.model.Category;
 import com.s_service.s_service.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +22,12 @@ public class CategoryServiceImpl implements CategoryService {
         Category category = categoryMapper.toCategory(request);
         category = categoryRepository.save(category);
         return categoryMapper.toGetCategoryResponse(category);
+    }
+
+    @Override
+    public List<CategoryResponse> getAllCategoriesWithServices() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream().map(categoryMapper::toCategoryResponse
+        ).toList();
     }
 }

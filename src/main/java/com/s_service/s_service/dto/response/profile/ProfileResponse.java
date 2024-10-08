14acf2f1875c0c2b38066da6fd93a736417216c6
quocthai <lxlthailxl@gmail.com>
2024-10-08 15:55:42 +0700

    update profile res dto

--- src/main/java/com/s_service/s_service/dto/response/profile/ProfileResponse.java
+++ src/main/java/com/s_service/s_service/dto/response/profile/ProfileResponse.java
@@ -1,11 +1,14 @@
 package com.s_service.s_service.dto.response.profile;
 
+import com.s_service.s_service.model.Account;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class ProfileResponse {
+    private String id;
+
     private String name;
 
     private String email;
@@ -13,4 +16,6 @@ public class ProfileResponse {
     private String phone;
 
     private String address;
+
+    private Account.AccountStatus status;
 }

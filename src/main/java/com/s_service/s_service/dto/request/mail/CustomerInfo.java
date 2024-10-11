0da46740052433e1b0c2479b7b03a3a5e94becea
quocthai <lxlthailxl@gmail.com>
2024-10-11 10:31:34 +0700

    add customer info dto for handle contact request

--- src/main/java/com/s_service/s_service/dto/request/mail/CustomerInfo.java
+++ src/main/java/com/s_service/s_service/dto/request/mail/CustomerInfo.java
@@ -1,2 +1,13 @@
-package com.s_service.s_service.dto.request.mail;public class CustomerInfo {
+package com.s_service.s_service.dto.request.mail;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerInfo {
+    private String name;
+    private String email;
 }
